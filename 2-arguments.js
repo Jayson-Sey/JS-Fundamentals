9ef3d0e1613a9608c18f5cb57577bed49ddd5f60
Jayson <jaysonsey005@gmail.com>
2025-09-12 23:34:31 +0200

    Arguments

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const totalArgs = process.argv.length - 2;
+
+if (totalArgs === 0) {
+	console.log("No argument");
+} else if (totalArgs === 1) {
+	console.log("Argument found");
+} else {
+	console.log("Argument found");
+}
